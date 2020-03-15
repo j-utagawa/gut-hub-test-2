dc81596b9109cbf3617a6c8f1eca887022ed4da2
c7a7r7r7r7a7 <61942875+c7a7r7r7r7a7@users.noreply.github.com>
2020-03-15 23:44:00 +0900

    Initial Commit

---
+++ gut hub test 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gut hub test 2
+//
+//  Created by 歌川純平 on 2020/03/15.
+//  Copyright © 2020 歌川純平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ gut hub test 2Tests/gut_hub_test_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  gut_hub_test_2Tests.swift
+//  gut hub test 2Tests
+//
+//  Created by 歌川純平 on 2020/03/15.
+//  Copyright © 2020 歌川純平. All rights reserved.
+//
+
+import XCTest
+@testable import gut_hub_test_2
+
+class gut_hub_test_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
